ed3097976a9c30ae799e28e633800f3c2510ea15
kassing3 <108312371+kassing3@users.noreply.github.com>
2022-09-11 13:58:13 -0400

    JavaScript: Add question and options

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,27 +3,17 @@ const highScoresbtn = document.getElementById('high-scores-btn')
 const nextBtn = document.getElementById('next-btn')
 const submitBtn = document.getElementById('submit-btn')
 
-const instructSection = document.querySelectorAll('#instructions')
+const instructSection = document.querySelector('#instructions')
 const quizSection = document.getElementById('quiz-game')
 const resultsSection = document.getElementById('results')
 const highScoresSection = document.getElementById('high-scores')
 
 const countdown = document.getElementById('countdown')
 const question = document.getElementById('question')
-const choices = querySelectorAll('.choices')
+const choices = document.querySelector('.choices-list')
 const highScoreInitial = document.getElementById('initials')
+const highScoreResult = document.getElementById('message-score')
 
-function setTimer() {
-    let timeLeft = 60;
-    let timerInterval = setInterval(function() {
-        timeLeft--;
-        countdown.textContent = timeLeft;
-
-        if (timeLeft === 0) {
-            clearInterval(timerInterval);
-        }
-    }, 1000);
-}
 
 
 let questionsObject = [
@@ -47,32 +37,88 @@ let questionsObject = [
         answer: "Undefined",
         options: ["Undefined", "Null", "Error", "False"]
     },
+
     {questionNumb: 5,
-        question: "What is a function called that do not have a name value?",
+        question: "What is a function called that do NOT have a name value?",
         answer: "Anonymous Functions",
         options: ["Undefined Function", "Anonymous Functions", "Arrow Functions", "Expressions"]
     },
 ];
 
-function showQuestions(index) {
-    question.textContent = questionsObject[index][1];
-    index++
+function setTimer() {
+    let timeLeft = 60;
+    let timerInterval = setInterval(function() {
+        timeLeft--;
+        countdown.textContent = timeLeft;
 
-} 
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            resultsSection.classList.remove('hidden');
+            quizSection.classList.add('hidden');
+        };
+
+    }, 1000);
+};
+
+
 
 function startGame(event) {
-    setTimer();
-    showQuestions(0);
     instructSection.classList.add('hidden');
     quizSection.classList.remove('hidden');
-
+    showQuestions();
+    setTimer();
 };
+
+let index = 0;
+let score = 0;
+
+function showQuestions() {
+    question.textContent = questionsObject[index].question;
+    
+    for (let i = 0; i < 4; i++) {
+        let choiceEl = document.createElement('li');
+        choiceEl.textContent = questionsObject[index].options[i];
+        choiceEl.classList.add('choices');
+        choices.appendChild(choiceEl);
+
+        choiceEl.addEventListener('click', function() {
+            if (choiceEl.textContent === questionsObject[index].answer) {
+                choiceEl.style.backgroundColor = 'green';
+                score = score + 5
+                console.log(score);
+            } else {
+                choiceEl.style.backgroundColor = 'red';
+                if (score > 0) {
+                    score = score - 5;
+                    console.log(score);
+                };  
+            };  
+        });
+    };
+
+    nextBtn.addEventListener('click', function () {
+        while (choices.hasChildNodes()) {
+            choices.removeChild(choices.lastChild);
+            };
+        index++;
+        showQuestions();
+    });
+
+    highScoreResult.textContent = score;
+
+ };
+
+ 
 
 startBtn.addEventListener('click', startGame);
 
+// submitBtn.addEventListener('click', function () {   
+//     let savedInitials = initials.value;
+//     localStorage.setItem("savedInitials", JSON.stringify(savedInitials));
+//     localStorage.setItem(score, JSON.stringify(score));
+// });
 
-function saveHighScore() {
-   let savedInitials = initials.value;
-    localStorage.setItem("savedInitials", JSON.stringify(savedInitials));
-
-}
+// highScoresbtn.addEventListener('click' function() {
+//     instructSection.classList.add('hidden');
+//     highScoresSection.classList.remove('hidden')
+// })
